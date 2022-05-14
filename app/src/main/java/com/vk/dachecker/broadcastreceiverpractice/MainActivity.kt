6146526d1e4b634a54acf30be01e6b37d8d5c449
dachecker1 <dachecker@ya.ru>
2022-05-14 17:03:08 +0300

    передаю собственное событие в broadcastReceiver

--- app/src/main/java/com/vk/dachecker/broadcastreceiverpractice/MainActivity.kt
+++ app/src/main/java/com/vk/dachecker/broadcastreceiverpractice/MainActivity.kt
@@ -3,24 +3,40 @@ package com.vk.dachecker.broadcastreceiverpractice
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val receiver = Receiver()
+    private var clickCounter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intentFilterAirPlane = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-        val intentFilterBatteryLow = IntentFilter(Intent.ACTION_BATTERY_LOW)
-        registerReceiver(receiver, intentFilterAirPlane)
-        registerReceiver(receiver, intentFilterBatteryLow)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            clickCounter++
+            val intent = Intent(Receiver.ACTION_CLICKED)
+            intent.putExtra(CLICK_COUNTER, clickCounter)
+            sendBroadcast(intent)
+        }
+
+
+        val intentFilter = IntentFilter().apply {
+            addAction(Intent.ACTION_BATTERY_LOW)
+            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+            addAction(Receiver.ACTION_CLICKED)
+        }
+        registerReceiver(receiver, intentFilter)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(receiver)
 
+    }
+
+    companion object {
+        const val CLICK_COUNTER = "click_counter"
     }
 }
--- app/src/main/java/com/vk/dachecker/broadcastreceiverpractice/Receiver.kt
+++ app/src/main/java/com/vk/dachecker/broadcastreceiverpractice/Receiver.kt
@@ -9,12 +9,21 @@ class Receiver : BroadcastReceiver() {
 
     override fun onReceive(p0: Context?, p1: Intent?) {
         when (p1?.action) {
+            ACTION_CLICKED -> {
+                val count = p1.getIntExtra(MainActivity.CLICK_COUNTER, 0)
+                Toast.makeText(p0, "Click Counter: $count", Toast.LENGTH_SHORT).show()
+            }
             Intent.ACTION_AIRPLANE_MODE_CHANGED -> {
-                Toast.makeText(p0, "Mode Airplane have been changed", Toast.LENGTH_LONG).show()
+                val isModeOn = p1.getBooleanExtra("state", false)
+                Toast.makeText(p0, "Airplane mode on: $isModeOn", Toast.LENGTH_LONG).show()
             }
             Intent.ACTION_BATTERY_LOW -> {
                 Toast.makeText(p0, "Battery low", Toast.LENGTH_LONG).show()
             }
         }
+    }
+
+    companion object {
+        const val ACTION_CLICKED = "clicked"
     }
 }
